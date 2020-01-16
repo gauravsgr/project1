81c954076ea4dc0da0746e42bbe3cdd625cf52c5
gauravsgr <gaurav.sgr@gmail.com>
2020-01-15 23:56:18 -0800

    Added in functionality to add reviews

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Project 1
 
-Web Programming with Python and JavaScript
+## Web Programming with Python and JavaScript
 
-Steps to install and run:
+## Objections
+In this project, I have build a book review website. Users will be able to register for my website and then log in using their username and password. Once they log in, they will be able to search for books, leave reviews for individual books, and see the reviews made by other people. I’ll also use the a third-party API by Goodreads, another book review website, to pull in ratings from a broader audience. Finally, users will be able to query for book details and book reviews programmatically via my website’s API.
+
+## Requirements
+1. Registration: Users should be able to register for my website, providing (at minimum) a username and password.
+2. Login: Users, once registered, should be able to log in to my website with their username and password.
+3. Logout: Logged in users should be able to log out of the site.
+4. Import: Provided in this project is a file called books.csv, which is a spreadsheet in CSV format of 5000 different books. Each one has an ISBN number, a title, an author, and a publication year. In a Python file called import.py separate from my web application, write a program that will take the books and import them into my PostgreSQL database. Decide what table(s) to create, what columns those tables should have, and how they should relate to one another. Run this program by running python3 import.py to import the books into my database, and submit this program with the rest of my project code.
+5. Search: Once a user has logged in, they should be taken to a page where they can search for a book. Users should be able to type in the ISBN number of a book, the title of a book, or the author of a book. After performing the search, my website should display a list of possible matching results, or some sort of message if there were no matches. If the user typed in only part of a title, ISBN, or author name, my search page should find matches for those as well!
+6. Book Page: When users click on a book from the results of the search page, they should be taken to a book page, with details about the book: its title, author, publication year, ISBN number, and any reviews that users have left for the book on my website.
+7. Review Submission: On the book page, users should be able to submit a review: consisting of a rating on a scale of 1 to 5, as well as a text component to the review where the user can write their opinion about a book. Users should not be able to submit multiple reviews for the same book.
+8. Goodreads Review Data: On my book page, I should also display (if available) the average rating and number of ratings the work has received from Goodreads.
+9. API Access: If users make a GET request to my website’s /api/<isbn> route, where <isbn> is an ISBN number, my website should return a JSON response containing the book’s title, author, publication date, ISBN number, review count, and average score. The resulting JSON should follow the format:
+
+
+## Steps to install and run:
 1. Run pip3 install -r requirements.txt OR install anaconda and run conda install --file requirements.txt
 2. Set the environment variable FLASK_APP to be application.py
    export FLASK_APP=application.py
@@ -13,6 +28,4 @@ Steps to install and run:
 5. Set the goodreadskey as an envoirnment variable to hold the key from the Goodreads API dashboard
    export goodreadskey=SomethingNotThatLong
 6. Run flask
-
-
 
--- application.py
+++ application.py
@@ -2,10 +2,11 @@ import os
 import psycopg2
 import requests
 
-from flask import Flask, session, render_template, jsonify, request
+from flask import Flask, session, render_template, jsonify, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from datetime import date
 
 app = Flask(__name__)
 
@@ -23,9 +24,11 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+
 @app.route("/")
 @app.route("/index.html")
 def index():
+    """The home page method"""
     if 'user' in session:
         return render_template("search.html")
     return render_template("index.html")
@@ -62,52 +65,68 @@ def authenticate():
 
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
-    if 'user' in session:
-        session.pop('user', None) # delete user info from session
-        return render_template("index.html")
+    """"The Logout clears session and returns to index page"""
+    session.clear()
+    return redirect(url_for('index'))       
 
 
 
 @app.route("/search", methods=["POST", "GET"])
 def search():
+    """Return the list of books that match the user search query even partially to title, author, isbn or year"""
     bookstr = request.form.get("bookstring")
-    
     # Get all the books that match bookstring
-    # search_string = "SELECT * FROM books WHERE isbn LIKE " + "'\%" + bookstr + "\%'" + " OR WHERE title LIKE " + "'\%" + bookstr + "\%'" + " OR WHERE author LIKE " + "'\%" + bookstr + "\%'"
-    search_string = "SELECT * FROM books WHERE author = '" + bookstr +"'"
+    search_string = "SELECT * FROM books WHERE isbn LIKE " + "'%" + bookstr + "%'" + " OR title LIKE " + "'%" + bookstr + "%'" + " OR author LIKE " + "'%" + bookstr + "%'"
     books = db.execute(search_string).fetchall()
-    
     return render_template("search.html", books=books, user = session['user'])
 
 
-@app.route("/bookinfo/<string:isbn>", methods=["GET"])
+@app.route("/bookinfo/<string:isbn>", methods=["GET", "POST"])
 def get_book_info_page(isbn):
-    
+    """Getting the book details based on ISBN and showing to the user"""
     res = requests.get("http://127.0.0.1:5000/api/bookinfo/" + isbn)
-    title = res.json()["title"]
-    author = res.json()["author"]
-    year = res.json()["year"]
-    isbn = res.json()["isbn"]
-    review_count = res.json()["review_count"]
-    average_score = res.json()["average_score"]
-    #print(title + '|' + author + '|' + year + '|' + isbn + '|' + review_count + '|' + average_score) """
-    return render_template("bookinfo.html", title=title, author=author, year=year, isbn=isbn, review_count=review_count, average_score=average_score)
+    reviews = db.execute("SELECT * FROM reviews where isbn = :isbn", 
+                                    {"isbn": isbn}).fetchall()
+    bookinfo = []
+    bookinfo.append(res.json()["title"])
+    bookinfo.append(res.json()["author"])
+    bookinfo.append(res.json()["year"])
+    bookinfo.append(res.json()["isbn"])
+    bookinfo.append(res.json()["review_count"])
+    bookinfo.append(res.json()["average_score"])
+    bookinfo.append(reviews)
+    # session['bookinfo'] = bookinfo
+    # return render_template("bookinfo.html", bookinfo = session["bookinfo"]) //Storing bookinfo in session was causing the race condition. 
+    return render_template("bookinfo.html", bookinfo = bookinfo)
 
 
 
-@app.route("/api/bookinfo/<string:isbn>", methods=["GET"])
+@app.route("/add_review/<string:isbn>", methods=["POST", "GET"])
+def add_review(isbn):
+    """Add the review of the book in the database to show to all users"""
+    username = session["user"]
+    review = request.form.get("review")
+    # If user already has a review then do nothing
+    preview = db.execute("SELECT * FROM reviews where uname = :username and isbn = :isbn", 
+                                    {"username": username, "isbn": isbn}).fetchone()
+    # Checking is the user has already posted a review
+    if preview is None:
+        db.execute("INSERT INTO reviews (isbn, uname, review, date) VALUES (:isbn, :uname, :review, :date)",
+                {"isbn": isbn, "uname": username, "review": review, "date": str(date.today())}) 
+        db.commit() 
+    return redirect(url_for('get_book_info_page', isbn = isbn))       
+
+
+
+@app.route("/api/bookinfo/<string:isbn>", methods=["GET", "POST"])
 def get_book_info(isbn):
     """Return details of a single book"""
-
     book = db.execute("SELECT * FROM books where ISBN = :isbn",
                         {"isbn":isbn}).fetchone()
-
     # Make sure book exists.    
     if book is None:
         return jsonify({"error": "Invalid book ISBN"}), 422
-
     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": os.getenv("goodreadskey"), "isbns": isbn})
-    
     with app.app_context():
         resp =  jsonify({
                 "title": book.title,
@@ -120,14 +139,12 @@ def get_book_info(isbn):
     return resp
 
 
-def test():
-    print("Hello World")
-
 
 def main():
+    """Test function"""
     print(get_book_info('5559609129')) #valid book isbn
     #print(get_book_info('12')) #invalid book isbn
-    #print(test())
+
 
 
 if __name__ == "__main__":
